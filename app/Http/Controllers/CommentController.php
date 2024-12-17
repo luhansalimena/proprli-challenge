c52eb15698ad09cae1da679084b508c1265d2694
Luhan Salimena <luhan@beepayapp.com.br>
2024-12-17 20:46:35 -0300

    Refactor CommentController to use CreateComment action for storing comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Tasks\CreateComment;
 use App\Http\Requests\StoreCommentRequest;
 use App\Models\Task;
-use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     public function store(StoreCommentRequest $request,Task $task)
     {
-        $task->comments()->create($request->validated());
-        return $task->load('comments');
+        return CreateComment::execute($request->validated(),$task);
     }
 }
