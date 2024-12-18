4933e7f72d892e8f9ad0dc1730f80aa6608c78ab
Luhan Salimena <luhan@beepayapp.com.br>
2024-12-18 00:16:01 -0300

    Bind TasksRepositoryInterface to TasksRepository in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repository\Interface\TasksRepositoryInterface;
+use App\Repository\TasksRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TasksRepositoryInterface::class, TasksRepository::class);
     }
 
     /**
