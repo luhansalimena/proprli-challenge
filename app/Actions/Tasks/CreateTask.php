0c8918ce47016781546d6baed0a003daac77ff88
Luhan Salimena <luhan@beepayapp.com.br>
2024-12-18 00:25:16 -0300

    Refactor CreateTask action to use TasksRepositoryInterface for task creation

--- app/Actions/Tasks/CreateTask.php
+++ app/Actions/Tasks/CreateTask.php
@@ -3,12 +3,25 @@
 namespace App\Actions\Tasks;
 
 use App\Models\Task;
-
+use App\Repository\Interface\TasksRepositoryInterface;
 
 class CreateTask
 {
+    private TasksRepositoryInterface $repository;
+
+    public function __construct(TasksRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public static function execute(array $data): Task
     {
-        return Task::create($data);
+        $instance = app()->make(self::class);
+        return $instance->handle($data);
+    }
+
+    public function handle(array $data): Task
+    {
+        return $this->repository->create($data);
     }
 }
